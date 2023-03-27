8f21bff0d68e318263da4900876ec957e9c10b70
EeroLai <84887419+EeroLai@users.noreply.github.com>
2023-03-27 22:42:04 +0800

    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app-root/app.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSidenavModule 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
