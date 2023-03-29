2207daad7657b6d8b91572c9f6574afc34b0bb32
EeroLai <84887419+EeroLai@users.noreply.github.com>
2023-03-30 00:12:02 +0800

    update example

--- src/app/components/app-root/app.component.ts
+++ src/app/components/app-root/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getUser } from 'src/app/services/api/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+
+  //example
+  test = getUser(1);
 }
--- src/app/services/api/user.service.ts
+++ src/app/services/api/user.service.ts
@@ -1,12 +1,13 @@
 import { ApiClient } from './api-client.service';
 import { User } from 'src/app/models/user.model';
 
+ 
 const apiClient = new ApiClient();
 
-async function getUser(id: number): Promise<User> {
+export async function getUser(id: number): Promise<User> {
     return apiClient.get<User>(`/users/${id}`);
 }
 
-async function updateUser(user: User): Promise<User> {
+export async function updateUser(user: User): Promise<User> {
     return apiClient.put<User>(`/users/${user.id}`, user);
 }
